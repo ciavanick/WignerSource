3260cc9f27122af12c45b86757928a2a601c81d8
Niccolò Ciavarelli <152533242+ciavanick@users.noreply.github.com>
2025-06-15 22:13:07 +0200

    added dictionary configuration to doxyfile

--- WignerLinkDef.h
+++ WignerLinkDef.h
@@ -1,8 +1,16 @@
-#ifdef __CLING__
-#pragma link off all globals;
-#pragma link off all classes;
-#pragma link off all functions;
+/** 
+ * \file WignerLinkDef.h
+ * \brief ROOT dictionary configuration for wignerSource and wignerUtils.
+ * 
+ * This file contains `#pragma link` directives used by ROOT's CINT/Cling 
+ * to generate class dictionaries for I/O and introspection.
+ */
 
-#pragma link C++ class wignerSource+;
-#pragma link C++ class wignerUtils+;
-#endif
+ #ifdef __CLING__
+ #pragma link off all globals;   ///< Disable global variable linking
+ #pragma link off all classes;   ///< Disable class linking by default
+ #pragma link off all functions; ///< Disable function linking by default
+ 
+ #pragma link C++ class wignerSource+; ///< Enable ROOT dictionary for wignerSource
+ #pragma link C++ class wignerUtils+;  ///< Enable ROOT dictionary for wignerUtils
+ #endif
